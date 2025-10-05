61d790085a0c268158fe4f0941388deb8e450014
GENEZIO DE LAVOR OLIVEIRA <gegeitp@gmail.com>
2025-10-05 08:35:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5690450065-ca94e",
+  "appId": "1:207208901439:web:3f7bd3e8b9db09fbca418a",
+  "apiKey": "AIzaSyCEhYheIk_8kVHuH-Mx5aewwy2LvsQ0pxo",
+  "authDomain": "studio-5690450065-ca94e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "207208901439"
+};
